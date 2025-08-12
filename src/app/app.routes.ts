0f2f3608996a8737d0074ea4a1bf3ca3712d3d13
Yesid Sanabria <yesanabria2@poligran.edu.co>
2025-08-12 17:50:09 -0500

    Enrutamineto e implementacion de tratamientos de datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos';
+import { PoliticaGarantiaComponent } from './pages/politica-garantia/politica-garantia';
+import { PoliticaTratamientoDatosComponent } from './pages/politica-tratamiento-datos/politica-tratamiento-datos';
+import { ProveedoresComponent } from './pages/proveedores/proveedores';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quienes-somos', component: QuienesSomosComponent },
+    { path: 'politica-garantia', component: PoliticaGarantiaComponent },
+    { path: 'politica-tratamiento-datos', component: PoliticaTratamientoDatosComponent },
+    { path: 'proveedores', component: ProveedoresComponent },
+];
