57d9907ca35d502868fff806b26d4f4dc7d5f736
Yesid Sanabria <yesanabria2@poligran.edu.co>
2025-08-12 13:37:59 -0500

    V1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { ContactForm } from "./components/contact-form/contact-form";
+import { Gallery } from "./components/gallery/gallery";
+import { Hero } from "./components/hero/hero";
+import { HeaderComponent } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, ContactForm, Gallery, Hero, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,38 @@
+import { Component, HostListener, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrls: ['./header.css']
+})
+export class HeaderComponent {
+  isDropdownOpen = false;
+
+  // Productos que irán en el menú
+  products = [
+    { name: 'Granito', link: '#productos' },
+    { name: 'Quarzita', link: '#productos' },
+    { name: 'Mármol', link: '#productos' },
+    { name: 'Piedra Bali', link: '#productos' },
+    { name: 'Lava Stone', link: '#productos' },
+    { name: 'Areniscas', link: '#productos' },
+    { name: 'Travertino', link: '#productos' },
+    { name: 'Calizas', link: '#productos' },
+    { name: 'Ónix', link: '#productos' },
+    { name: 'Mosaicos', link: '#productos' }
+  ];
+
+  constructor(private eRef: ElementRef) {}
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // Cierra el menú si se hace clic fuera de él
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if(!this.eRef.nativeElement.contains(event.target)) {
+      this.isDropdownOpen = false;
+    }
+  }
+}
