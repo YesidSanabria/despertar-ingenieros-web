f618c9e33b1a019f2801a9047f24c5c344bb96c7
Yesid Sanabria <yesanabria2@poligran.edu.co>
2025-08-15 12:17:16 -0500

    Actualizacion links

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -9,7 +9,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), 
+    provideRouter(routes,
+      withInMemoryScrolling({
+        // Esta opción fuerza a la página a ir al inicio en cada navegación
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    ), 
     provideHttpClient(),
     provideClientHydration(withEventReplay())
   ]
